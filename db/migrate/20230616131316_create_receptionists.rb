662148d4402563bf520729c87313d64f82d92e44
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-16 16:19:35 +0300

    test:add receptionists migration

---
+++ db/migrate/20230616131316_create_receptionists.rb
@@ -0,0 +1,15 @@
+class CreateReceptionists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :receptionists do |t|
+  t.string :first_name
+      t.string :last_name
+      t.integer :phone
+      t.string :email
+      t.string :password_digest
+      t.string :designation
+      t.string :role_s
+
+      t.timestamps
+    end
+  end
+end
